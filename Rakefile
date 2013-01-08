5c8bc4e999a54ae2edead1875e72ebad89184607
Christopher Jones <christopher@sweetspotdiabetes.com>
2013-01-08 15:15:21 -0800

    todos for adding configuration

--- Rakefile
+++ Rakefile
@@ -7,11 +7,19 @@ require 'rubygems'
 # start up sauce-connect
 task :setup_sauce_connect do
   puts "About to set up Sauce Connect"
+  # add git submodule for sauce-connect
+  # execute sauce-connect with current configs
+  # java -jar Sauce-Connect.jar cjones_sweetspot 121e5b2c-262c-4d6e-913b-3f143f8a8470
 end
 
 # run tests
 task :run_sauce_tests =>[:setup_sauce_connect] do
   # set up tests
+
+  # add actual sauce configuration
+  # CapybaraSauce.add_sauce_conf
+
+  puts "TODO: load config"
   # browsers = ENV['browsers']
   # oss = ENV['oss']
 end
--- lib/capybara_sauce.rb
+++ lib/capybara_sauce.rb
@@ -2,4 +2,49 @@ class CapybaraSauce
   def self.hi
     puts "hello there!"
   end
+
+  # add sauce configuration to rspec/rspec_helper.rb
+  def self.add_sauce_conf
+    # TODO: is there a testing machine we're planning on running from?
+    # require 'capybara/rspec'
+    # require 'capybara/rails'
+    # require 'sauce'
+    # require 'sauce/capybara'
+
+    # Sauce.config do |conf|
+    #   # conf['username'] = "cj@sweetspotdiabetes.com"
+    #   conf['username'] = "cjones_sweetspot"
+    #   conf['accessKey'] = "121e5b2c-262c-4d6e-913b-3f143f8a8470"
+    #   conf['name'] = "CJ testing"
+    #   # conf['local_application_port'] = 3001
+
+    #   # acceptable browsers/os combos
+    #   # https://saucelabs.com/docs/browsers
+
+    #   # sauce config opts.  Actually, this is mostly wrong.
+    #   # http://rdoc.info/github/saucelabs/sauce_ruby/Sauce/Config
+
+    #   # is vista not supported by sauce?
+
+    #   # XP/FF16
+    #   conf['platform'] = "Windows 2003"
+    #   conf['browserName'] = "Firefox"
+    #   conf['version'] = "16"
+
+    #   # XP/IE8
+    #   # conf['platform'] = "Windows 2003"
+    #   # conf['browserName'] = "Internet Explorer"
+    #   # conf['version'] = "8"
+
+    #   # Win7/IE9
+    #   # conf['platform'] = "Windows 2008"
+    #   # conf['browserName'] = "Internet Explorer"
+    #   # conf['version'] = "9"
+    # end
+
+    # Rails.logger.debug("big config: #{config.inspect}") #CDJ
+
+    # Capybara.default_driver = :sauce
+
+  end
 end
